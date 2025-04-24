7985e520d7886f8ff0974ad7c0a43646e83b16ea
Lens Balan <lensrambinho21@gmail.com>
2025-04-24 11:42:38 -0300

    modificações...

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ let input = document.querySelector('input[name=Tarefa]');
 */
 let botao = document.querySelector('#botao');
 //console.log(botao);
+
 /*
 Lista
 */
@@ -30,6 +31,10 @@ let tarefas = [
 ];
 
 function adicionar_tarefas(){
+
+    //limpar antes de adicionar
+    lista.innerHTML = '';
+
     for(tarefa of tarefas){
         //criar item da lista
         let itemLista = document.createElement('li');
@@ -41,8 +46,46 @@ function adicionar_tarefas(){
         itemLista.appendChild(itemTexto);
         //Adicionar o item da lista na lista global
         lista.appendChild(itemLista);
-
     }
 }
 
 adicionar_tarefas();
+
+//1 - Capturar o clique do botão
+//2 - recuperar o valor digitado no input 
+botao.onclick = function(){
+    let novatarefa = input.value;
+
+    if(novatarefa !== ""){
+        tarefas.push(novatarefa);
+
+        adicionar_tarefas();
+
+        //limpar input a cada nova adição
+        input.value = '';
+        //limpar msgns de erro
+        remover_spans();
+    }else{
+        //limpar msgns de erro
+        remover_spans();
+        
+        let card = document.querySelector('.card');
+        
+        let span = document.createElement('span');
+        span.setAttribute('class', 'alert alert-warning');
+
+        let msg = document.createTextNode('Você precisa informar a tarefa!');
+
+        span.appendChild(msg);
+        card.appendChild(span);     
+    }
+}
+
+function remover_spans(){
+    let spans = document.querySelectorAll('span');
+    let card = document.querySelector('.card');
+
+    for(let i = 0; i < spans.length; i++){
+        card.removeChild(spans[i]);
+    }
+}
